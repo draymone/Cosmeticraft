763f02c346fae38f91cd54e095e530109c34f042
draymone <socotcho@gmail.com>
2023-09-14 23:25:02 +0200

    Adds commonSetup and creates a logger

--- src/main/java/com/draymone/cosmeticraft/Cosmeticraft.java
+++ src/main/java/com/draymone/cosmeticraft/Cosmeticraft.java
@@ -5,19 +5,25 @@ import com.draymone.cosmeticraft.init.CreativeTabInit;
 import com.draymone.cosmeticraft.init.ItemInit;
 import com.draymone.cosmeticraft.init.client.PebbleRenderer;
 import com.draymone.cosmeticraft.init.entity.ModBlockEntities;
+import com.draymone.cosmeticraft.networking.ModMessages;
+import com.mojang.logging.LogUtils;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderers;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
 import software.bernie.geckolib.GeckoLib;
 
 @Mod(Cosmeticraft.MODID)
 public class Cosmeticraft {
 
     public static final String MODID = "cosmeticraft";
+    public static final Logger LOGGER = LogUtils.getLogger();
 
     public Cosmeticraft() {
         IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
@@ -27,7 +33,19 @@ public class Cosmeticraft {
         CreativeTabInit.TABS.register(bus);
         ModBlockEntities.register(bus);
 
+        bus.addListener(this::commonSetup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+
         GeckoLib.initialize();
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+        event.enqueueWork(() -> {
+
+        });
+
+        ModMessages.register();
     }
 
     // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
